6563b9983d418fe7e4415c822ab19b040e765ef0
nisamudheen <nisamudheen@localhost.localdomain>
2021-01-12 02:02:00 +0530

    make it better new server

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@ const mongoClient = require('mongodb').MongoClient
 const state={
     db:null
 }
-const mongodb ="mongodb+srv://nisamozr:TRradk1nGzwceVGx@cluster0.gk83i.mongodb.net/classroom?retryWrites=true&w=majority"
+// const mongodb ="mongodb+srv://nisamozr:TRradk1nGzwceVGx@cluster0.gk83i.mongodb.net/classroom?retryWrites=true&w=majority"
 module.exports.connect=function(done){
-    const url=process.env.MONGODB_URI||mongodb||'mongodb://localhost:27017'
+    const url="mongo --host bngc6mqgckavvqg-mongodb.services.clever-cloud.com --port 27017 --username=uueisfbgwlkbkzrusxwc bngc6mqgckavvqg"||'mongodb://localhost:27017'
     const dbname = 'classroom'
-
+console.log(process.env.MONGODB_URI)
     mongoClient.connect(url,(err,data)=>{
         if(err) return done(err)
         state.db = data.db(dbname)
